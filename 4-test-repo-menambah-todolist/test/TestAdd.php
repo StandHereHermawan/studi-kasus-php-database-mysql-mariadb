76f1536a1ecc5f12707e194f864fb92c7b226dc8
Arief-Karditya <nolifegansaya@gmail.com>
2024-08-01 15:22:12 +0700

    Test Repo Menambah Todolist

---
+++ 4-test-repo-menambah-todolist/test/TestAdd.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once __DIR__ . "/../config/Koneksi.php";
+require_once __DIR__ . "/../entity/Todolist.php";
+require_once __DIR__ . "/../repository/TodolistRepository.php";
+require_once __DIR__ . "/../service/TodolistService.php";
+
+use Repository\TodolistRepositoryImpl;
+use Service\TodolistServiceImpl;
+use Config4\Database;
+
+##[Test]
+function testAddTodolist(): void
+{
+    $connection = Database::getConnection();
+    $todolistRepository = new TodolistRepositoryImpl($connection);
+
+    $todolistService = new TodolistServiceImpl($todolistRepository);
+    $todolistService->addToDoList("Belajar PHP di PZN");
+    $todolistService->addToDoList("Belajar Laravel di PZN");
+    $todolistService->addToDoList("Belajar Bootstrap di PZN");
+}
+
+testAddTodolist();
